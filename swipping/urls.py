1ae44600096ab27aa44d734db81e650946bc2e2e
GhostsDragons <harigoyal2003@gmail.com>
2023-11-01 23:45:40 +0530

    swipping_pam

--- swipping/urls.py
+++ swipping/urls.py
@@ -20,6 +20,6 @@ from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("swipping/", views.swiping, name="matching"),
+    path("", views.swipping, name="matching"),
 
 ]
--- swipping/views.py
+++ swipping/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def swiping(request):
-    return render(request, "swiping_pam.html")
+def swipping(request):
+    return render(request, "swipping_pam.html")
