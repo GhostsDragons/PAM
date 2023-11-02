bd30e3c270caa2c56c7ed3ad207e0e12327aea24
GhostsDragons <harigoyal2003@gmail.com>
2023-11-02 14:11:49 +0530

    trial

--- login/models.py
+++ login/models.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User, AbstractUser
 # Create your models here.
 
 
-class UserProfile(models.Model):
+class User(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
